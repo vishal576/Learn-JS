dfa9bc9cf7f2abf892c23bc7fd0fdecde3023bf4
vishal576 <110327491+vishal576@users.noreply.github.com>
2025-11-01 00:25:36 +0530

    1 basic color changer project complete

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,28 @@
 console.log('Vishal');
+
+// 4 js projects with chai or code 
+
+let body = document.querySelector("body");
+let button = document.querySelectorAll(".button");
+
+button.forEach(function (button){
+    console.log(button);
+    button.addEventListener("click", function(e){
+        console.log(e);
+        // console.log(e.target);
+      if(e.target.id === "blue"){
+        body.style.backgroundColor = e.target.id;
+      }
+      if(e.target.id === "red"){
+        body.style.backgroundColor = e.target.id;
+      }
+      if(e.target.id === "yellow"){
+        body.style.backgroundColor = e.target.id;
+      }
+      if(e.target.id === "pink"){
+        body.style.backgroundColor = e.target.id;
+      }
+        
+    })
+   
+})
